d06681c789157d8f73b1885df587e5f6e4d6f378
meriem meriem <chamimeriem2302@gmail.com>
2024-07-16 19:30:33 +0100

    fix data

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -3,13 +3,16 @@ import { MenuData } from "../Data/Data";
 
 const Menu = ({ title }) => {
   const Data = MenuData.filter((item) => item.category === title);
+
   return (
     <div className=" flex flex-col justify-center items-center w-full pt-36 px-5 h-full md:pb-5">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 text-center py-8 justify-center items-center">
-        {" "}
         {Data.map(({ id, src, name, price }) => {
           return (
-            <div key={id} className="flex flex-row border-b border-white py-5 w-80 md:w-96">
+            <div
+              key={id}
+              className="flex flex-row border-b border-white py-5 w-80 md:w-96"
+            >
               <img
                 src={src}
                 alt=""
@@ -25,6 +28,18 @@ const Menu = ({ title }) => {
             </div>
           );
         })}
+        {title === "Crêpes & Gaufres" || title === "Pancakes" ? (
+          <div className="flex gap-2">
+            <p className="havana-font font-bold text-four">
+              Supplément Boule De Glace
+            </p>
+            <p className="font-bold">+ 150 da</p>
+          </div>
+        ) : title === "Jus Pressé" ? (
+          <p className="havana-font font-bold text-four">
+            (deux fruits au choix)
+          </p>
+        ) : null}
       </div>
     </div>
   );
