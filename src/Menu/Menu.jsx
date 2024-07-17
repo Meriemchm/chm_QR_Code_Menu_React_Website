40cf7ed1b2efd4b15ba654d40934f3d6785514bb
meriem meriem <chamimeriem2302@gmail.com>
2024-07-17 12:35:27 +0100

    fiw width pic

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -5,8 +5,8 @@ const Menu = ({ title }) => {
   const Data = MenuData.filter((item) => item.category === title);
 
   return (
-    <div className=" flex flex-col justify-center items-center w-full pt-36 px-5 h-full md:pb-5">
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 text-center py-8 justify-center items-center">
+    <div className=" flex flex-col justify-center md:items-center items-start w-full pt-36 md:px-5 px-2 h-full md:pb-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 w-full md:w-auto text-center py-8 justify-center items-center">
         {Data.map(({ id, src, name, price }) => {
           return (
             <div
@@ -16,11 +16,11 @@ const Menu = ({ title }) => {
               <img
                 src={src}
                 alt=""
-                className="rounded-md w-36 md:w-44 h-32 object-cover mt-5"
+                className="rounded-md w-44 h-32 object-cover mt-5"
               />
 
               <div className="flex flex-col items-start justify-start text-gray-800 mx-5 my-4 ">
-                <h2 className="havana-font text-bold text-base md:text-lg font-bold capitalize py-2 text-four">
+                <h2 className="havana-font  text-bold text-base md:text-lg font-bold capitalize py-2 text-four">
                   {name}
                 </h2>
                 <p className="font-bold">{price}</p>
