7ef8092294e7c071bf3fdac5ee0ec95612bb607e
meriem meriem <chamimeriem2302@gmail.com>
2024-07-17 12:26:31 +0100

    fiw width pic

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -16,7 +16,7 @@ const Menu = ({ title }) => {
               <img
                 src={src}
                 alt=""
-                className="rounded-md w-44 h-32 object-cover mt-5"
+                className="rounded-md w-36 md:w-44 h-32 object-cover mt-5"
               />
 
               <div className="flex flex-col items-start justify-start text-gray-800 mx-5 my-4 ">
