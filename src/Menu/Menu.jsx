dca6dd00b06d0e4e1871341ce4e43621430f07a6
meriem meriem <chamimeriem2302@gmail.com>
2024-08-21 14:40:22 +0100

    fix text menu widt

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -5,7 +5,7 @@ const Menu = ({ title }) => {
   const Data = MenuData.filter((item) => item.category === title);
 
   return (
-    <div className=" flex flex-col justify-center md:items-center items-start w-full pt-36 md:px-5 px-2 h-full md:pb-5">
+    <div className=" flex flex-col justify-center md:items-center items-start w-full pt-36 md:px-5 px-1 h-full md:pb-5">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 w-full md:w-auto text-center py-8 justify-center items-center">
         {Data.map(({ id, src, name, price }) => {
           return (
@@ -20,11 +20,11 @@ const Menu = ({ title }) => {
                 loading="lazy"
               />
 
-              <div className="flex flex-col items-start justify-start text-gray-800 mx-5 my-4 ">
-                <h2 className="havana-font  text-bold text-base md:text-lg font-bold capitalize py-2 text-four">
+              <div className="flex flex-col items-start justify-start text-gray-800 md:mx-5 my-4 ">
+                <p className="havana-font  text-bold text-base md:text-lg font-bold capitalize ml-2 md:ml-0  py-2 text-four">
                   {name}
-                </h2>
-                <p className="font-bold">{price}</p>
+                </p>
+                <p className="font-bold mx-2 md:mx-0">{price}</p>
               </div>
             </div>
           );
