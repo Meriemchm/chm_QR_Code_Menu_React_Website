ab9742e836571d54ea7db847d76dc96f5b54a51e
meriem meriem <chamimeriem2302@gmail.com>
2024-08-21 12:22:13 +0100

    add loading=lazy

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -17,6 +17,7 @@ const Menu = ({ title }) => {
                 src={src}
                 alt=""
                 className="rounded-md w-44 h-32 object-cover mt-5"
+                loading="lazy"
               />
 
               <div className="flex flex-col items-start justify-start text-gray-800 mx-5 my-4 ">
